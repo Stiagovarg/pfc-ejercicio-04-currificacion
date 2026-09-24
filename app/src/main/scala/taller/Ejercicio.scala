263f01aba74b0054dd5f3971b8d10e9515626658
stiagov <santiago.alfonso.vargas@correounivalle.edu.co>
2026-09-24 15:24:24 -0500

    main

--- app/src/main/scala/taller/Ejercicio.scala
+++ app/src/main/scala/taller/Ejercicio.scala
@@ -17,13 +17,13 @@ class Ejercicio() {
 
   // Punto 2. La suma de la sesión con tres grupos de parámetros.
   def suma4(f: Int => Int)(prox: Int => Int)(a: Int, b: Int): Int = {
-
+    0
 
   }
 
   // suma4 con f y prox ya fijados: cuadrados de uno en uno.
   def sumaCuadradosSuc: (Int, Int) => Int = {
-    (a, b) =>  sumaCuadradosSuc(suma,suma4(a:Int = a*a)__)// Completar con una aplicación parcial de suma4
+    (a,b)=>0// Completar con una aplicación parcial de suma4
   }
 
   // Punto 3. La operación y su valor inicial en los dos primeros grupos.
